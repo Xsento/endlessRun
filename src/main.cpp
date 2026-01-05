3779cbf47052b8e0bdf5c0f92ec6c3d1fe5cffbe
erqover <matiwareda@gmail.com>
2026-01-05 19:37:33 +0100

    Testing: sprites, textures, events sprite following mouse

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
+// system headers
+#include <iostream>
+#include <math.h>
+// lib headers
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+// project headers
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode({600, 600}), "SFML works!");
     window.setFramerateLimit(60);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
+    sf::Texture texture("assets/erqu_works_small.jpg");
+    sf::Sprite sprite(texture);
+    sprite.setPosition({100.f, 300.f});
+    int direction = 1;
+    float ypos = 0;
 
     while (window.isOpen())
     {
@@ -13,10 +26,16 @@ int main()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+            // if (const auto* mouseMoved = event->getIf<sf::Event::MouseMoved>()) {
+            //     std::cout << "new mouse x: " << mouseMoved->position.x << std::endl;
+            //     std::cout << "new mouse y: " << mouseMoved->position.y << std::endl;
+            // }
+            if (const auto* mouse = event->getIf<sf::Event::MouseMoved>()) {
+                sprite.setPosition({static_cast<float>(mouse->position.x), static_cast<float>(mouse->position.y)});
+            }
         }
-
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 }
